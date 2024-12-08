86918963fa7ad46172bf511173e9f531cf15283d
CHOCL02 <20031563@itcelaya.edu.mx>
2024-12-08 13:52:38 -0600

    Agregar tareas inicio

--- TaskTracker.py
+++ TaskTracker.py
@@ -1,3 +1,4 @@
+"""
 print("---------MENU---------")
 print("Seleccione una opción:")
 print("1. Listar Tareas")
@@ -5,3 +6,31 @@ print("2. Agregar Tareas")
 print("3. Actualizar Tareas")
 print("4. Eliminar Tareas")
 
+respuesta = input()
+"""
+
+def agregar_tareas(tareas):
+    cantidad_tareas = input("¿Cuántas tareas deseas agregar? ")
+    contador = 0
+    while contador < int(cantidad_tareas):
+        contador += 1
+        descripcion = input("Descripción: ")
+        # Agregar la descripción de la tarea
+        tareas[contador] = {"descripcion": descripcion}
+        print("Define el estado de la tarea:")
+        print("1. To do")
+        print("2. In progress")
+        status = input("Estado: ")
+        # Actualizar el estado de la tarea
+        if int(status) == 1:
+            tareas[contador]["status"] = "todo"
+        else:
+            tareas[contador]["status"] = "in-progress"
+    print("Tareas cargadas\n")
+
+tareas = {}
+agregar_tareas(tareas)
+print("Listado de tareas:")
+print(tareas[1])
+for id_tarea, detalles in tareas.items():
+    print(f"Tarea {id_tarea}: Descripción: {detalles['descripcion']}, Estado: {detalles['status']}")
